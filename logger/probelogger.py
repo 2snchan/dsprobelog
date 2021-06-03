6019c9a02278d5690241f95e6de47da74e937004
2snchan <leesc030720@naver.com>
2021-06-03 14:16:25 +0900

    Refactor & Comments

--- logger/probelogger.py
+++ logger/probelogger.py
@@ -5,6 +5,8 @@ import requests
 from scapy.all import *
 from key import codekey
 
+last_update = time.time()-120
+
 #
 # 2021 DS Project - Probelogger
 # Catches IEEE 802.11 proberequest signal and log / dispatch to server
@@ -13,8 +15,10 @@ from key import codekey
 # scapy and netaddr library need to be installed.
 #
 
-last_update = time.time()-120
-
+# This function update informations to server (ksadensity.com/inbound.php)
+# Code means beacon number, which represent location of beacon.
+# There are certain time wifi connect provided, 6AM to 0AM, so I put if branch to code distinct it.
+# Uses requests & datetime lib
 def update(value):
 	code = 2
 	baseURL = "https://ksadensity.com/inbound.php?id="
@@ -36,8 +40,11 @@ def update(value):
 
 	return
 
-
+# Device class store information of catched devices
+# Data is stored in dict, keyvalue is MAC address.
+# __init__, __str__, insert, update, calculate, printlist method
 class device():
+	# __init__, get option values = interval, rssi_threshold, rtime(residence time)
 	def __init__(self, interval, rssi_threshold, rtime):
 		self.data = dict()
 		self.number = 0
@@ -45,47 +52,47 @@ class device():
 		self.rssi_threshold = rssi_threshold
 		self.rtime = rtime
 
+	# __str__, return number of registered devices	
 	def __str__(self):
 		return self.number
 
+	# insert, push device information in data dictonary
 	def insert(self, time, macaddr, target, name, rssi_value):
 		#if target != 'KSA'.encode() and target != 'KSA_SUB'.encode:
 		#	return
-
 		if rssi_value < self.rssi_threshold :
 			return
-		
 		if macaddr in self.data:
 			self.data[macaddr][0] = time
 		else:
 			self.data[macaddr] = [time, target, name, rssi_value]
-
 		return
 	
+	# update, check device information is still vaild, as axis of time.
 	def update(self):
 		L = []
 		interval = self.interval
-		
 		for key in self.data:
 			if time.time() - int(self.data[key][0]) > interval :
 				print("Device  " + str(key) +"  Depeted")
 				L.append(key)
-		
 		for i in L:
 			del self.data[i]
-
 		return
 	
+	# calculate, Count number of devices
 	def calculate(self):
 		self.number = len(self.data)
 		return self.number
 
+	# printlist, If option = True print all device's information
 	def printlist(self):
 		for key in self.data:
 			print("Device Name : " + str(self.data[key][2][0:10]) + " Rssi_Value : "+str(self.data[key][3]))
 		return
 
-
+# build_packet_callback, callback function handle sniffed packet.
+# It parses scapy packet and get rssi, mac address, organization.
 def build_packet_callback(device, option):
 	def packet_callback(packet):
 		global last_update
@@ -123,6 +130,7 @@ def build_packet_callback(device, option):
 	return packet_callback
 
 
+# main function, config values and start logging
 def main():
 	#subprocess.call('sudo rmmod r8188eu.ko',shell=True)
 	#subprocess.call('sudo ifconfig wlx7cc2c6026fb5 down',shell=True)
